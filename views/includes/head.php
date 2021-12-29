1aad3f6b3f20e52e26cc594d53e2ca8451895b19
Anderson Ouattara <anderson.ouattara@inexa-ci.com>
2021-12-29 11:31:15 +0000

    feat: Mise en place de la structure MVC

---
+++ views/includes/head.php
@@ -0,0 +1,29 @@
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="Author" content="AndOs">
+  <meta name="Description" content="Application web de gestion de ferme avicole">
+  <link rel="icon" type="image/x-icon" href="assets/images/favicon.png"/>
+  <link rel="shortcut icon" type="image/x-icon" href="assets/images/favicon.png"/>
+
+  <!-- Google Font: Source Sans Pro -->
+  <!-- <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback"> -->
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="plugins/fontawesome-free/css/all.min.css">
+  <!-- Ionicons -->
+  <!-- <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css"> -->
+  <!-- Tempusdominus Bootstrap 4 -->
+  <link rel="stylesheet"
+        href="plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css">
+  <!-- iCheck -->
+  <link rel="stylesheet" href="plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="css/templates/adminlte.min.css">
+  <!-- overlayScrollbars -->
+  <link rel="stylesheet" href="plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
+  <!-- Daterange picker -->
+  <link rel="stylesheet" href="plugins/daterangepicker/daterangepicker.css">
+
+  <!-- Mes fichiers style CSS  -->
+  <link rel="stylesheet" href="css/main.css">
+</head>
